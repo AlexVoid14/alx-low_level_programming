cf812b646002b3f934d8618bf70f02f8a464b267
AlexVoid14 <fortuinalexander48@gmail.com>
2022-12-09 09:23:20 -0800

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,11 +8,11 @@ int main(void)
 	int tens;
 	int ones;
 
-	for (tens = 0; tens = 9; tens++)
-		for (ones = tens; ones = 9; ones++)
+	for (tens = 0; tens <= 9; tens++)
+		for (ones = tens; ones <= 9; ones++)
 			putchar(tens + '0');
 	putchar(ones + '0');
-	if (tens < 8)
+	if (tens < 9)
 		putchar(',');
 	putchar(' ');
 	putchar('\n');
