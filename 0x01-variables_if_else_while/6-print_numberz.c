1879145bd8dd21610e23f6dc291f53e0f99990a5
AlexVoid14 <fortuinalexander48@gmail.com>
2022-12-09 04:20:46 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers in base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num + '0');
+	putchar('\n');
+	return (0);
+}
