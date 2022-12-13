3aeea52aea0fcdc6f8a11642cd331b3b4fc8e801
AlexVoid14 <fortuinalexander48@gmail.com>
2022-12-13 01:39:40 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main -  prints the alphabet in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lo;
+
+	for (lo = 'a'; lo <= 'z'; lo++)
+		_putchar(lo);
+
+	putchar('\n');
+
+	return (0);
+}
