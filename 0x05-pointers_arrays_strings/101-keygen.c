8f7d400ee718b079ee07133ca8c79c32b88ef465
AlexVoid14 <fortuinalexander48@gmail.com>
2022-12-20 07:51:31 -0800

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,10 +13,10 @@ int main(void)
 	sum = 0;
 	while (sum <= 2465)
 	{
-		passs = (rand() % 128);
+		pass = (rand() % 128);
 		sum += pass;
 		printf("%c", pass);
 	}
 	printf("%c", 2772 - sum);
-	retrun(0);
+	return (0);
 }
