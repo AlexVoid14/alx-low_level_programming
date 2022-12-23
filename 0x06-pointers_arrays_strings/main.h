43242d9c0db7e50f983dd16066228216f809a583
AlexVoid14 <fortuinalexander48@gmail.com>
2022-12-23 11:31:21 -0800

    task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,8 +3,8 @@
 #include <stdio.h>
 /**
  * void prototypes()
- * int prototype()
- * char prototype()
+ * int prototypes()
+ * char prototypes()
  */
 void reverse_array(int *a, int n);
 void print_number(int n);
