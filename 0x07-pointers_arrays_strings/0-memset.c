e16fe5eb057849a342139f3fa776117fbb4313b9
AlexVoid14 <fortuinalexander48@gmail.com>
2023-01-03 07:48:08 -0800

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fills memorywith a constant byte
+ * @s: source string
+ * @b: constant byte
+ * @n: length of buffer
+ * Return: new string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int 1 = 0;
+
+	while(i < 1)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return(s);
+}
