6cbf65b05d3574b0693bca84061e932e73e80c7c
AlexVoid14 <fortuinalexander48@gmail.com>
2023-01-09 06:29:19 -0800

    task 0

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fills memorywith a constant byte
+ * @s: source string
+ * @b: constant byte
+ * @n: length of buffer
+ * Return: new string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - cpoies the string pointed and terminate null
+ * @dest: is a destroy
+ * @src: a copia
+ * Return: return copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
