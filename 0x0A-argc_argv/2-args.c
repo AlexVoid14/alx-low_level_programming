7cf9106d42306b804618fb040b6d15dad123d542
AlexVoid14 <fortuinalexander48@gmail.com>
2023-01-09 08:03:05 -0800

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,7 +4,7 @@
  * main - prints all arguments program recieves.
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: Always 0
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
