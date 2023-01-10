ff39116627fa7adca63d1e9d50549ace2471834a
AlexVoid14 <fortuinalexander48@gmail.com>
2023-01-10 04:38:40 -0800

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,9 +18,9 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	arr = malloc(sizeof(char *size);
-			/*check if malloc was successful*/
-			if (arr == NULL)
+	arr = malloc(sizeof(char) * size);
+	/*check if malloc was successful*/
+	if (arr == NULL)
 			return (NULL);
 
 	for (i = 0; i < size; i++)
