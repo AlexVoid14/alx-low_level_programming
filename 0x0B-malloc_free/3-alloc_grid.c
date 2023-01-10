4fbbe47941bda0fe0d25acf4fac5f84fde72d11c
AlexVoid14 <fortuinalexander48@gmail.com>
2023-01-10 05:38:42 -0800

    task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - creates the matrix using malloc
+ * @width: width of the matrix
+ * @height: height of the matrix
+ * Return: pointer of a pointer or NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, a, b;
+	int **p;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		p = (int **) malloc(height * sizeof(int *));
+		/*we have to make maloc per pointer*/
+		if (!p)
+		{
+			free(p);
+			return (NULL);
+		}
+		for (a = 0; a < height; a++)
+		{
+			for (b = 0; b < width; b++)
+			{
+				p[a][b] = 0;
+			}
+		}
+		return (p);
+	}
+}
