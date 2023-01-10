5e043eaf7fa74bc678c080673d5b746b7b872abd
AlexVoid14 <fortuinalexander48@gmail.com>
2023-01-10 05:28:23 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,7 +28,7 @@ char *str_concat(char *s1, char *s2)
 		len2++;
 	size = len1 + len2;
 
-	nstr + malloc((sizeof(char) * size) + 1);
+	nstr = malloc((sizeof(char) * size) + 1);
 	/*check if malloc was successful*/
 	if (nstr == NULL)
 		return (NULL);
