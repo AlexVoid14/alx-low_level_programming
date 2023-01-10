4f0971cb87abf7c6e087e523148774b9956f5a36
AlexVoid14 <fortuinalexander48@gmail.com>
2023-01-10 04:32:13 -0800

    task 0

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,7 @@
+void free_grid(int **grid, int height);
+char *str_concat(char *s1, *s2);
+char *_strdup(char *str);
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+int *alloc_grid(int width, int height);
+chaar *argstostr(int ac, char **av);
