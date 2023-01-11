d0204d703a32d474852d39414ea993ca2f468c71
AlexVoid14 <fortuinalexander48@gmail.com>
2023-01-11 06:21:56 -0800

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -21,7 +21,7 @@ int main(int argc, int *argv[])
 			str = argv[i];
 			for (b = 0; str[b] != '\0'; b++)
 			{
-				if (str[b] < 48; || str[b] > 57)
+				if (str[b] < 48 || str[b] > 57)
 				{
 					printf("Error\n");
 					return (1);
