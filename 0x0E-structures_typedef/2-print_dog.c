4dcb5af9238c62ecd7ecdc3918e211f98de452d7
AlexVoid14 <fortuinalexander48@gmail.com>
2023-01-16 04:28:15 -0800

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,7 +4,7 @@
 /**
  * print_dog - prints a struct dog
  * @d: pointer to struct
- *Return: void
+ * Return: void
  */
 void print_dog(struct dog *d)
 {
@@ -18,6 +18,7 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
+
 		printf("Age: %f\n", d->age);
 
 		if (d->owner == NULL)
