2bd280f3071d52b45b373b11e3c711ac265f35df
AlexVoid14 <fortuinalexander48@gmail.com>
2023-01-18 02:48:16 -0800

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,7 +4,7 @@
  * @array: array of elements
  * @size: size of array
  * @action: function pointer
- * Return: void
+ *Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int));
 {
